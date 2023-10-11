d0998bad3ba3a362e8bb09ddafb487511f5fd16a
Catiflam <gilfrid1@gmail.com>
2023-10-11 19:09:55 +0200

    edit hello word

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+
+    $title = 'Hello Word';
+
+    return view('home',compact('title'));
 });
